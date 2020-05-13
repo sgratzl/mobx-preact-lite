cdd95a33f2dbd09c983558cdf8b95f6c8fd7dc79
Samuel Gratzl <sam@sgratzl.com>
2020-05-13 13:50:41 +0200

    restructure to copy files

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import "./assertEnvironment";
-export { isUsingStaticRendering, useStaticRendering } from "./staticRendering";
-export { observer, IObserverOptions } from "./observer";
-export { useObserver, ForceUpdateHook, IUseObserverOptions } from "./useObserver";
-export { Observer } from "./ObserverComponent";
-export { useForceUpdate } from "./utils";
-export { useAsObservableSource } from "./useAsObservableSource";
-export { useLocalStore } from "./useLocalStore";
-export { observerBatching, observerBatchingOptOut, isObserverBatched } from "./observerBatching";
--- dist/printDebugValue.d.ts
+++
@@ -1,2 +0,0 @@
-import { Reaction, IDependencyTree } from "mobx";
-export declare function printDebugValue(v: Reaction): IDependencyTree;
